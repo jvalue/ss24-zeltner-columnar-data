faa7dcbb68f5625741a15ccbe0eca2d7a1e0fa3c
Jonas Zeltner <jonas.zeltner@posteo.de>
2024-04-14 13:59:45 +0200

    proof of concept reproduction of the cars example

--- read_csv.ts
+++ read_csv.ts
@@ -1,7 +1,51 @@
 import process from "node:process";
 
-import pl from "nodejs-polars";
+import pl, { DataFrame } from "nodejs-polars";
+import { Database } from "sqlite3";
+
+function getColsString(
+  df: pl.DataFrame,
+  f: (col: pl.Series) => string,
+): string {
+  let col_names: string[] = [];
+  for (const col of df.getColumns()) {
+    col_names.push(f(col));
+  }
+  return col_names.join(", ");
+}
+
+function createTable(db: Database, df: DataFrame, name: string) {
+  const nameAndDType = (col: pl.Series) => {
+    return `${col.name}`;
+  };
+  db.run(
+    `CREATE TABLE IF NOT EXISTS cars (${getColsString(df, nameAndDType)})`,
+  );
+}
+
+function insertValues(db: Database, df: DataFrame, table_name: string) {
+  const name = (col: pl.Series) => {
+    return `${col.name}`;
+  };
+  let values = [];
+  for (const row of df.rows()) {
+    let r = [];
+    for (const cell of row) {
+      r.push(`${cell}`);
+    }
+    values.push(`(${row.join(", ")})`);
+  }
+  const query = `INSERT INTO ${table_name} (${getColsString(df, name)}) VALUES ${values.join(", ")}`;
+  console.log(query);
+  db.run(query);
+}
+
+function writeSQLite(df: pl.DataFrame, table: string) {
+  const db = new Database("./db.sqlite");
+  createTable(db, df, table);
+  insertValues(db, df, table);
+}
 
 let path = process.argv[2];
-const csv = pl.readCSV(path);
-console.log(csv);
+const df = pl.readCSV(path);
+writeSQLite(df, "cars");
