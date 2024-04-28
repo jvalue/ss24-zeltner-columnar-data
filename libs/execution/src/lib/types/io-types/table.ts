dada71b4b3c4058fbf1f1361db8e221275af5700
Jonas Zeltner <jonas.zeltner@posteo.de>
2024-04-28 12:29:28 +0200

    WIP

--- libs/execution/src/lib/types/io-types/table.ts
+++ libs/execution/src/lib/types/io-types/table.ts
@@ -19,6 +19,7 @@ import {
   type IOTypeImplementation,
   type IoTypeVisitor,
 } from './io-type-implementation';
+import { zip, zipWith } from 'fp-ts/lib/ReadonlyNonEmptyArray';
 
 export interface TableColumn<
   T extends InternalValueRepresentation = InternalValueRepresentation,
@@ -114,8 +115,11 @@ export class PolarsTable extends AbstractTable {
     });
     return m;
   }
-  override getRow(rowId: number): Map<string, any> {
-    throw new Error('Method not implemented.');
+  override getRow(rowId: number): Map<string, InternalValueRepresentation> {
+    const rs = this.df.row(rowId);
+    const cnames = this.df.columns;
+    const zs = zip(cnames, rs);
+    return zs;
   }
   override generateInsertValuesStatement(tableName: string): string {
     throw new Error('Method not implemented.');
@@ -131,9 +135,7 @@ export class PolarsTable extends AbstractTable {
   }
 }
 
-export class Table implements IOTypeImplementation<IOType.TABLE> {
-  public readonly ioType = IOType.TABLE;
-
+export class TsTable implements AbstractTable {
   private numberOfRows = 0;
 
   private columns = new Map<string, TableColumn>();
