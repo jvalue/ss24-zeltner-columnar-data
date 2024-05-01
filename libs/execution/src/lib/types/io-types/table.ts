7d1a031d86e86caba465ee07e2bb95094f6fe4cc
Jonas Zeltner <jonas.zeltner@fau.de>
2024-05-01 07:47:15 +0200

    remove the import breaking everything

--- libs/execution/src/lib/types/io-types/table.ts
+++ libs/execution/src/lib/types/io-types/table.ts
@@ -21,7 +21,6 @@ import {
   type IOTypeImplementation,
   type IoTypeVisitor,
 } from './io-type-implementation';
-import { AbstractValueType } from 'libs/language-server/src/lib/ast/wrappers/value-type/abstract-value-type';
 
 export interface TableColumn<
   T extends InternalValueRepresentation = InternalValueRepresentation,
