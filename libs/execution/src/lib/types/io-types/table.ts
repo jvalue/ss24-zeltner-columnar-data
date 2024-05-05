98e953ea34494b0ba651baea5d82bd41c2d13b48
Jonas Zeltner <jonas.zeltner@fau.de>
2024-05-05 16:01:17 +0200

    pl and ts tables can be interpreted

--- libs/execution/src/lib/types/io-types/table.ts
+++ libs/execution/src/lib/types/io-types/table.ts
@@ -9,26 +9,37 @@ import {
   IOType,
   type InternalValueRepresentation,
   type PolarsAtomicInternalValueRepresentation,
-  PolarsInternalValueRepresentation,
   type TsInternalValueRepresentation,
   type ValueType,
 } from '@jvalue/jayvee-language-server';
-import { zip, zipWith } from 'fp-ts/lib/ReadonlyNonEmptyArray';
 import { DataType, pl } from 'nodejs-polars';
+
+import {
+  SQLColumnTypeVisitor,
+  SQLValueRepresentationVisitor,
+} from '../value-types/visitors/';
 
 import {
   type IOTypeImplementation,
   type IoTypeVisitor,
 } from './io-type-implementation';
+import { type } from 'os';
+
+export enum TableImpl {
+  Polars,
+  Typescript,
+}
 
 export interface TableColumn {
   getValueType(): ValueType;
   getName(): string;
   as_array(): readonly InternalValueRepresentation[];
   nth(n: number): InternalValueRepresentation | undefined;
+  getImpl(): TableImpl;
 }
 
 export abstract class AbstractTableColumn implements TableColumn {
+  abstract getImpl(): TableImpl;
   abstract getValueType(): ValueType<InternalValueRepresentation>;
   abstract getName(): string;
   abstract as_array(): readonly InternalValueRepresentation[];
@@ -58,6 +69,10 @@ export class PolarsTableColumn extends AbstractTableColumn {
     }
     return undefined;
   }
+
+  override getImpl(): TableImpl {
+    return TableImpl.Polars;
+  }
 }
 
 export class TsTableColumn<
@@ -86,42 +101,109 @@ export class TsTableColumn<
   override nth(n: number): T | undefined {
     return this.values.at(n);
   }
+
+  override getImpl(): TableImpl {
+    return TableImpl.Typescript;
+  }
 }
+
+export type TsTableRow = Record<string, TsInternalValueRepresentation>;
 
 /**
  * Invariant: the shape of the table is always a rectangle.
  * This means all columns must have the same size.
  */
 
-export interface Table extends IOTypeImplementation<IOType.TABLE> {
-  withColumn(column: TableColumn): Table;
-  filter<F>(cond: F): Table;
-  getNumberOfRows(): number;
-  getNumberOfColumns(): number;
-  hasColumn(name: string): boolean;
-  getColumns(): ReadonlyArray<TableColumn>;
-  getColumn(name: string): TableColumn | undefined;
-  getRow(id: number): InternalValueRepresentation[];
-  clone(): Table;
-  acceptVisitor<R>(visitor: IoTypeVisitor<R>): R;
-}
+// export interface Table extends IOTypeImplementation<IOType.TABLE> {
+//   withColumn(column: TableColumn): Table;
+//   filter<F>(cond: F): Table;
+//   getNumberOfRows(): number;
+//   getNumberOfColumns(): number;
+//   hasColumn(name: string): boolean;
+//   getColumns(): ReadonlyArray<TableColumn>;
+//   getColumn(name: string): TableColumn | undefined;
+//   getRow(id: number): InternalValueRepresentation[];
+//   clone(): Table;
+//   acceptVisitor<R>(visitor: IoTypeVisitor<R>): R;
+//   getImpl(): TableImpl;
+// }
 
-export abstract class AbstractTable implements Table {
+export abstract class Table implements IOTypeImplementation<IOType.TABLE> {
   public readonly ioType = IOType.TABLE;
 
-  abstract withColumn(column: AbstractTableColumn): AbstractTable;
-  abstract filter<F>(cond: F): AbstractTable;
+  abstract withColumn(column: AbstractTableColumn): Table;
+  abstract filter<F>(cond: F): Table;
   abstract getNumberOfRows(): number;
   abstract getNumberOfColumns(): number;
   abstract hasColumn(name: string): boolean;
   abstract getColumns(): ReadonlyArray<AbstractTableColumn>;
   abstract getColumn(name: string): AbstractTableColumn | undefined;
   abstract getRow(id: number): InternalValueRepresentation[];
-  abstract clone(): AbstractTable;
+  abstract clone(): Table;
   abstract acceptVisitor<R>(visitor: IoTypeVisitor<R>): R;
+  abstract getImpl(): TableImpl;
+
+  static newEmpty(impl: TableImpl): Table {
+    switch (impl) {
+      case TableImpl.Polars:
+        return new PolarsTable(pl.DataFrame({}));
+      case TableImpl.Typescript:
+        return new TsTable(0);
+    }
+  }
+
+  static generateDropTableStatement(tableName: string): string {
+    return `DROP TABLE IF EXISTS "${tableName}";`;
+  }
+
+  generateInsertValuesStatement(tableName: string): string {
+    const valueRepresentationVisitor = new SQLValueRepresentationVisitor();
+
+    const columnNames = [
+      ...this.getColumns().map((c) => {
+        return c.getName();
+      }),
+    ];
+    const formattedRowValues: string[] = [];
+    for (let rowIndex = 0; rowIndex < this.getNumberOfRows(); ++rowIndex) {
+      const rowValues: string[] = [];
+      for (const columnName of columnNames) {
+        const column = this.getColumn(columnName);
+        const entry = column?.nth(rowIndex);
+        assert(entry !== undefined);
+        const formattedValue = column
+          ?.getValueType()
+          .acceptVisitor(valueRepresentationVisitor)(entry);
+        assert(formattedValue !== undefined);
+        rowValues.push(formattedValue);
+      }
+      formattedRowValues.push(`(${rowValues.join(',')})`);
+    }
+
+    const formattedColumns = columnNames.map((c) => `"${c}"`).join(',');
+
+    return `INSERT INTO "${tableName}" (${formattedColumns}) VALUES ${formattedRowValues.join(
+      ', ',
+    )}`;
+  }
+
+  generateCreateTableStatement(tableName: string): string {
+    const columnTypeVisitor = new SQLColumnTypeVisitor();
+
+    const columns = [...this.getColumns()];
+    const columnStatements = columns.map((column) => {
+      return `"${column.getName()}" ${column
+        .getValueType()
+        .acceptVisitor(columnTypeVisitor)}`;
+    });
+
+    return `CREATE TABLE IF NOT EXISTS "${tableName}" (${columnStatements.join(
+      ',',
+    )});`;
+  }
 }
 
-export class PolarsTable extends AbstractTable {
+export class PolarsTable extends Table {
   df: pl.DataFrame = pl.DataFrame();
 
   public constructor(df: pl.DataFrame) {
@@ -148,13 +230,13 @@ export class PolarsTable extends AbstractTable {
     }
   }
 
-  override filter<F>(_cond: F): Table {
+  override filter<F>(cond: F): PolarsTable {
     throw new Error('Method not implemented.');
   }
 
   override getColumns(): readonly PolarsTableColumn[] {
     const seriess = this.df.getColumns();
-    return seriess.map((s, _i, _ss) => {
+    return seriess.map((s) => {
       return new PolarsTableColumn(s);
     });
   }
@@ -180,12 +262,17 @@ export class PolarsTable extends AbstractTable {
   override clone(): PolarsTable {
     return new PolarsTable(this.df.clone());
   }
+
   override acceptVisitor<R>(visitor: IoTypeVisitor<R>): R {
     return visitor.visitPolarsTable(this);
   }
+
+  override getImpl(): TableImpl {
+    return TableImpl.Polars;
+  }
 }
 
-export class TsTable extends AbstractTable {
+export class TsTable extends Table {
   private numberOfRows = 0;
 
   private columns = new Map<string, TsTableColumn>();
@@ -195,9 +282,7 @@ export class TsTable extends AbstractTable {
     this.numberOfRows = numberOfRows;
   }
 
-  override withColumn<T extends TsInternalValueRepresentation>(
-    column: TsTableColumn<T>,
-  ): TsTable {
+  override withColumn(column: TsTableColumn): TsTable {
     assert(column.values.length === this.numberOfRows);
     const nt = this.clone();
     nt.columns.set(column.name, column);
@@ -206,6 +291,36 @@ export class TsTable extends AbstractTable {
 
   override filter<F>(cond: F): TsTable {
     throw new Error('Metho not implemented.');
+  }
+
+  /**
+   * Tries to add a new row to this table.
+   * NOTE: This method will only add the row if the table has at least one column!
+   * @param row data of this row for each column
+   */
+  addRow(row: TsTableRow): void {
+    const rowLength = Object.keys(row).length;
+    assert(
+      rowLength === this.columns.size,
+      `Added row has the wrong dimension (expected: ${this.columns.size}, actual: ${rowLength})`,
+    );
+    if (rowLength === 0) {
+      return;
+    }
+    assert(
+      Object.keys(row).every((x) => this.hasColumn(x)),
+      'Added row does not fit the columns in the table',
+    );
+
+    Object.entries(row).forEach(([columnName, value]) => {
+      const column = this.columns.get(columnName);
+      assert(column !== undefined);
+
+      assert(column.valueType.isInternalValueRepresentation(value));
+      column.values.push(value);
+    });
+
+    this.numberOfRows++;
   }
 
   addColumn(name: string, column: TsTableColumn): void {
@@ -276,5 +391,9 @@ export class TsTable extends AbstractTable {
 
   override acceptVisitor<R>(visitor: IoTypeVisitor<R>): R {
     return visitor.visitTsTable(this);
+  }
+
+  override getImpl(): TableImpl {
+    return TableImpl.Typescript;
   }
 }
--- libs/extensions/tabular/exec/src/lib/table-interpreter-executor.ts
+++ libs/extensions/tabular/exec/src/lib/table-interpreter-executor.ts
@@ -10,8 +10,10 @@ import {
   AbstractBlockExecutor,
   type BlockExecutorClass,
   type ExecutionContext,
+  PolarsTable,
   type Sheet,
-  Table,
+  type Table,
+  TsTable,
   implementsStatic,
   isValidValueRepresentation,
   parseValueToInternalRepresentation,
@@ -24,6 +26,7 @@ import {
   type ValuetypeAssignment,
   rowIndexToString,
 } from '@jvalue/jayvee-language-server';
+import pl from 'nodejs-polars';
 
 export interface ColumnDefinitionEntry {
   sheetColumnIndex: number;
@@ -32,8 +35,7 @@ export interface ColumnDefinitionEntry {
   astNode: ValuetypeAssignment;
 }
 
-@implementsStatic<BlockExecutorClass>()
-export class TableInterpreterExecutor extends AbstractBlockExecutor<
+export abstract class TableInterpeter extends AbstractBlockExecutor<
   IOType.SHEET,
   IOType.TABLE
 > {
@@ -43,11 +45,65 @@ export class TableInterpreterExecutor extends AbstractBlockExecutor<
     super(IOType.SHEET, IOType.TABLE);
   }
 
-  // eslint-disable-next-line @typescript-eslint/require-await
-  async doExecute(
-    inputSheet: Sheet,
+  protected deriveColumnDefinitionEntriesWithoutHeader(
+    columnDefinitions: ValuetypeAssignment[],
     context: ExecutionContext,
-  ): Promise<R.Result<Table>> {
+  ): ColumnDefinitionEntry[] {
+    return columnDefinitions.map<ColumnDefinitionEntry>(
+      (columnDefinition, columnDefinitionIndex) => {
+        const columnValuetype = context.wrapperFactories.ValueType.wrap(
+          columnDefinition.type,
+        );
+        assert(columnValuetype !== undefined);
+        return {
+          sheetColumnIndex: columnDefinitionIndex,
+          columnName: columnDefinition.name,
+          valueType: columnValuetype,
+          astNode: columnDefinition,
+        };
+      },
+    );
+  }
+
+  protected deriveColumnDefinitionEntriesFromHeader(
+    columnDefinitions: ValuetypeAssignment[],
+    headerRow: string[],
+    context: ExecutionContext,
+  ): ColumnDefinitionEntry[] {
+    context.logger.logDebug(`Matching header with provided column names`);
+
+    const columnEntries: ColumnDefinitionEntry[] = [];
+    for (const columnDefinition of columnDefinitions) {
+      const indexOfMatchingHeader = headerRow.findIndex(
+        (headerColumnName) => headerColumnName === columnDefinition.name,
+      );
+      if (indexOfMatchingHeader === -1) {
+        context.logger.logDebug(
+          `Omitting column "${columnDefinition.name}" as the name was not found in the header`,
+        );
+        continue;
+      }
+      const columnValuetype = context.wrapperFactories.ValueType.wrap(
+        columnDefinition.type,
+      );
+      assert(columnValuetype !== undefined);
+
+      columnEntries.push({
+        sheetColumnIndex: indexOfMatchingHeader,
+        columnName: columnDefinition.name,
+        valueType: columnValuetype,
+        astNode: columnDefinition,
+      });
+    }
+
+    return columnEntries;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  override async doExecute(
+    inputSheet: Sheet,
+    context: R.ExecutionContext,
+  ): Promise<R.Result<R.Table>> {
     const header = context.getPropertyValue(
       'header',
       context.valueTypeProvider.Primitives.Boolean,
@@ -115,20 +171,47 @@ export class TableInterpreterExecutor extends AbstractBlockExecutor<
     return R.ok(resultingTable);
   }
 
-  private constructAndValidateTable(
+  protected abstract constructAndValidateTable(
+    sheet: Sheet,
+    header: boolean,
+    columnEntries: ColumnDefinitionEntry[],
+    context: ExecutionContext,
+  ): Table;
+}
+
+@implementsStatic<BlockExecutorClass>()
+export class PolarsTableInterpreterExecutor extends TableInterpeter {
+  protected override constructAndValidateTable(
+    sheet: Sheet,
+    _header: boolean,
+    columnEntries: ColumnDefinitionEntry[],
+    _context: ExecutionContext,
+  ): PolarsTable {
+    const schema: { [key: string]: string } = {};
+    columnEntries.forEach((e) => {
+      schema[e.columnName] = e.valueType.getName();
+    });
+    const df = pl.DataFrame(sheet.getData(), { orient: 'row', schema: schema });
+    return new PolarsTable(df);
+  }
+}
+
+@implementsStatic<BlockExecutorClass>()
+export class TsTableInterpreterExecutor extends TableInterpeter {
+  protected override constructAndValidateTable(
     sheet: Sheet,
     header: boolean,
     columnEntries: ColumnDefinitionEntry[],
     context: ExecutionContext,
   ): Table {
-    const table = new Table();
+    const table = new TsTable();
 
     // add columns
     columnEntries.forEach((columnEntry) => {
-      table.addColumn(columnEntry.columnName, {
-        values: [],
-        valueType: columnEntry.valueType,
-      });
+      table.addColumn(
+        columnEntry.columnName,
+        new R.TsTableColumn(columnEntry.columnName, [], columnEntry.valueType),
+      );
     });
 
     // add rows
@@ -159,9 +242,9 @@ export class TableInterpreterExecutor extends AbstractBlockExecutor<
     sheetRowIndex: number,
     columnEntries: ColumnDefinitionEntry[],
     context: ExecutionContext,
-  ): R.TableRow | undefined {
+  ): R.TsTableRow | undefined {
     let invalidRow = false;
-    const tableRow: R.TableRow = {};
+    const tableRow: R.TsTableRow = {};
     columnEntries.forEach((columnEntry) => {
       const sheetColumnIndex = columnEntry.sheetColumnIndex;
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -203,59 +286,5 @@ export class TableInterpreterExecutor extends AbstractBlockExecutor<
       return undefined;
     }
     return parsedValue;
-  }
-
-  private deriveColumnDefinitionEntriesWithoutHeader(
-    columnDefinitions: ValuetypeAssignment[],
-    context: ExecutionContext,
-  ): ColumnDefinitionEntry[] {
-    return columnDefinitions.map<ColumnDefinitionEntry>(
-      (columnDefinition, columnDefinitionIndex) => {
-        const columnValuetype = context.wrapperFactories.ValueType.wrap(
-          columnDefinition.type,
-        );
-        assert(columnValuetype !== undefined);
-        return {
-          sheetColumnIndex: columnDefinitionIndex,
-          columnName: columnDefinition.name,
-          valueType: columnValuetype,
-          astNode: columnDefinition,
-        };
-      },
-    );
-  }
-
-  private deriveColumnDefinitionEntriesFromHeader(
-    columnDefinitions: ValuetypeAssignment[],
-    headerRow: string[],
-    context: ExecutionContext,
-  ): ColumnDefinitionEntry[] {
-    context.logger.logDebug(`Matching header with provided column names`);
-
-    const columnEntries: ColumnDefinitionEntry[] = [];
-    for (const columnDefinition of columnDefinitions) {
-      const indexOfMatchingHeader = headerRow.findIndex(
-        (headerColumnName) => headerColumnName === columnDefinition.name,
-      );
-      if (indexOfMatchingHeader === -1) {
-        context.logger.logDebug(
-          `Omitting column "${columnDefinition.name}" as the name was not found in the header`,
-        );
-        continue;
-      }
-      const columnValuetype = context.wrapperFactories.ValueType.wrap(
-        columnDefinition.type,
-      );
-      assert(columnValuetype !== undefined);
-
-      columnEntries.push({
-        sheetColumnIndex: indexOfMatchingHeader,
-        columnName: columnDefinition.name,
-        valueType: columnValuetype,
-        astNode: columnDefinition,
-      });
-    }
-
-    return columnEntries;
   }
 }
